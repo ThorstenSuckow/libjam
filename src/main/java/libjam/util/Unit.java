9adc0fa95ba455921566c7517a1bc3c8e271dc83
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-14 19:45:07 +0200

    feat: add Unit enumeration and package-info

---
+++ src/main/java/libjam/util/Unit.java
@@ -0,0 +1,41 @@
+package libjam.util;
+
+
+/**
+ * Enumeration of units used throughout libjam.
+ */
+public enum Unit {
+
+    /**
+     * pixel
+     */
+    METER,
+
+
+    /**
+     * pixel
+     */
+    PIXEL,
+
+    /**
+     * centimeter
+     */
+    CM,
+
+    /**
+     * millimeter
+     */
+    MM,
+
+    /**
+     * milliseconds
+     */
+    MS,
+
+    /**
+     * seconds
+     */
+    SECONDS
+
+
+}
---
+++ src/main/java/libjam/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Collection of Utilities.
+ *
+ * @since 1.0
+ * @author Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
+ */
+
+package libjam.util;
