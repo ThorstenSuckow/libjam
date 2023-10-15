97aa775fd6461fdf7cdee6554ff30393e983aae8
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-16 00:21:52 +0200

    feat: add conversion methods to Unit

--- src/main/java/libjam/util/Unit.java
+++ src/main/java/libjam/util/Unit.java
@@ -9,33 +9,77 @@ public enum Unit {
     /**
      * pixel
      */
-    METER,
+    METER(),
 
 
     /**
      * pixel
      */
-    PIXEL,
+    PIXEL(),
 
     /**
      * centimeter
      */
-    CM,
+    CM(),
 
     /**
      * millimeter
      */
-    MM,
+    MM(),
 
     /**
      * milliseconds
      */
-    MS,
+    MS(),
 
     /**
      * seconds
      */
-    SECONDS
+    SECONDS();
 
 
+    /**
+     * Returns the value translated to the unit given the specified scale.
+     *
+     * @param value
+     * @param from
+     * @param to
+     * @param scale
+     * @return
+     */
+    public static double transformScaleValueToUnit(
+            double value,
+            final Unit from,
+            final Unit to,
+            final double scale
+    ) {
+
+        switch (from) {
+
+            case METER:
+                switch (to) {
+                    case PIXEL:
+                        return value / scale;
+                    default:
+                        throw new UnsupportedOperationException("unit not supported");
+                }
+
+            case PIXEL:
+                switch (to) {
+                    case METER:
+                        return value * scale;
+                    default:
+                        throw new UnsupportedOperationException("unit not supported");
+                }
+
+            default:
+                throw new UnsupportedOperationException("unit not supported");
+        }
+
+    }
+
+
+    public static double toScreenY(int y, double referenceHeight) {
+        return referenceHeight - y;
+    }
 }
