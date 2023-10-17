84a4c39fed2a817897205a230a2168886a5e2cec
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-17 13:02:50 +0200

    docs: add code commentary

--- src/main/java/libjam/gfx/CanvasContext.java
+++ src/main/java/libjam/gfx/CanvasContext.java
@@ -1,22 +1,46 @@
 package libjam.gfx;
 
+
+/**
+ * The CanvasContext provides information about the current Canvas and its metrics.
+ * The purpose of this class is to pass information about the current dimensions of
+ * a given Canvas to Drawables, providing information about potential rendering bounds.
+ *
+ */
 public class CanvasContext {
 
+    /**
+     * The width of the Canvas represented by this Context-object.
+     */
     private int width;
 
+    /**
+     * The height of the Canvas represented by this Context-object.
+     */
     private int height;
-    public CanvasContext(int width, int height) {
 
+
+    /**
+     * Createa a new CanvasContext.
+     *
+     * @param width The pixel-width of the represented Canvas.
+     * @param height The pixel-height of the represented Canvas.
+     */
+    public CanvasContext(final int width, final int height) {
         this.width = width;
         this.height = height;
-
     }
 
-
+    /**
+     * @return The pixel-width of the Canvas represented by this Context-object.
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * @return The pixel-height of the Canvas represented by this Context-object.
+     */
     public int getHeight() {
         return height;
     }
