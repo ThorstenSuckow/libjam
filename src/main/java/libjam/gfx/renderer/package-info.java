838e46ca7f162414a43d46f091569a66aef9b691
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-18 23:57:46 +0200

    refactor: add package-info to gfx.renderer

---
+++ src/main/java/libjam/gfx/renderer/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Packs renderer-related implementations.
+ * Specifics are available via specific subpackages, such as "world" for
+ * physx.World-related rendering.
+ *
+ * @since 1.0
+ * @author Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
+ */
+package libjam.gfx.renderer;
