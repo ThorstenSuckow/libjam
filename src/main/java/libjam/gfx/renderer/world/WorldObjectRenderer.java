e988f91c500ae1c794c154041ec829d16740cf8c
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-18 23:48:43 +0200

    refactor: add renderer interface for WorldObjects

---
+++ src/main/java/libjam/gfx/renderer/world/WorldObjectRenderer.java
@@ -0,0 +1,11 @@
+package libjam.gfx.renderer.world;
+
+import libjam.gfx.renderer.ObjectRenderer;
+
+/**
+ * Renderer for WorldObject.
+ */
+public interface WorldObjectRenderer extends ObjectRenderer {
+
+
+}
