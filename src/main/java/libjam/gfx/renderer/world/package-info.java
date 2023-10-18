99e93182f6c51d087bef45e3607ee5a149facb76
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-18 23:53:09 +0200

    chore: add package-info to package renderer.world

---
+++ src/main/java/libjam/gfx/renderer/world/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package provides renderer implementations for creating visual representatives for objects of libjam.physx.World and
+ * related classes.
+ *
+ * @since 1.0
+ * @author Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
+ */
+package libjam.gfx.renderer.world;
