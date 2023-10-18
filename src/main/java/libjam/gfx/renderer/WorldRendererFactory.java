1795b8558fa01a55bc96e22e1f195dd7da83746b
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-18 23:59:13 +0200

    refactor: update signature for factory's getRenderer() and add docs

--- src/main/java/libjam/gfx/renderer/WorldRendererFactory.java
+++ src/main/java/libjam/gfx/renderer/WorldRendererFactory.java
@@ -1,9 +1,22 @@
 package libjam.gfx.renderer;
 
+import libjam.gfx.renderer.world.WorldObjectRenderer;
 import libjam.physx.WorldObject;
 
+/**
+ * Interface for renderer-factories for WorldObjects.
+ */
 public interface WorldRendererFactory {
 
-    ObjectRenderer getRenderer(WorldObject worldObj);
+
+    /**
+     * Returns the appropriate renderer applicable for the specified
+     * WorldObject.
+     *
+     * @param worldObject The worldObject that requires a renderer.
+     *
+     * @return A renderer for the specified WorldObject.
+     */
+    WorldObjectRenderer getRenderer(WorldObject worldObject);
 
 }
