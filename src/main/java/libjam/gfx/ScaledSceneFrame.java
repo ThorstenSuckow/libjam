a3925853470876923333abdba0a9c3b00c0e6791
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-20 18:05:48 +0200

    refactor: update ScaledSceneFrame and add tests

--- src/main/java/libjam/gfx/ScaledSceneFrame.java
+++ src/main/java/libjam/gfx/ScaledSceneFrame.java
@@ -1,24 +1,24 @@
 package libjam.gfx;
 
 /**
- * A SceneFrame that has its values scaled according to scalingFactor.
- *
- * <pre>
- *     +-----------------------------+
- *     |     +----------------+      |
- *     |     |                |      |
- *     |     |                |      |
- *     |     +--SceneFrame----+      |
- *     |                             |
- *     +----------Scene--------------+
- * </pre>
- *
- *
+ * SceneFrame providing a scaled view of a scene.
  */
-public class ScaledSceneFrame extends SceneFrame {
+final public class ScaledSceneFrame extends SceneFrame {
 
-    private double scalingFactor;
+    @SuppressWarnings("checkstyle:JavadocVariable")
+    private final double scalingFactor;
 
+
+    /**
+     * Creates a new ScaledSceneFrame with the specified scalingFactor.
+     *
+     * @param x The x-coordinate of the scene.
+     * @param y The y-coordinate of the scene.
+     * @param width The width of the scene.
+     * @param height The height of the scene.
+     * @param scalingFactor The scaling factor to use for this SceneFrame.
+     */
+    @SuppressWarnings("checkstyle:HiddenField")
     public ScaledSceneFrame(
             final double x,
             final double y,
@@ -32,10 +32,26 @@ public class ScaledSceneFrame extends SceneFrame {
     }
 
 
+    /**
+     * @return the scalingFactor used for this SceneFrame.
+     */
     public double getScalingFactor() {
         return scalingFactor;
     }
 
 
+    /**
+     * Checks the specified ScaledSceneFrame for equality with this ScaledSceneFrame. Two ScaledSceneFrame are
+     * considered equal if their x, y, width, height and scalingFactor attributes are the same.
+     *
+     * @param scaledSceneFrame The specified ScaledSceneFrame.
+     * @return true if the specified ScaledSceneFrame is considered equal to this ScaledSceneFrame, otherwise false.
+     */
+    public boolean equals(final ScaledSceneFrame scaledSceneFrame) {
+
+        return super.equals(scaledSceneFrame)
+            && scalingFactor == scaledSceneFrame.getScalingFactor();
+
+    }
 
 }
