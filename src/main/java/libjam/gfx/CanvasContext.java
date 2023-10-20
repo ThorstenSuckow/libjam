4f96c81c549c9fad5615fa465eb0cd319eb19f0f
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-20 18:06:30 +0200

    feat: add safeArea-information to CanvasContext and add tests

--- src/main/java/libjam/gfx/CanvasContext.java
+++ src/main/java/libjam/gfx/CanvasContext.java
@@ -1,23 +1,27 @@
 package libjam.gfx;
 
 
+import libjam.math.Rectangle;
+
 /**
  * The CanvasContext provides information about the current Canvas and its metrics.
  * The purpose of this class is to pass information about the current dimensions of
  * a given Canvas to Drawables, providing information about potential rendering bounds.
  *
  */
-public class CanvasContext {
+public final class CanvasContext {
 
-    /**
-     * The width of the Canvas represented by this Context-object.
-     */
+
+    @SuppressWarnings("checkstyle:JavadocVariable")
     private int width;
 
-    /**
-     * The height of the Canvas represented by this Context-object.
-     */
+
+    @SuppressWarnings("checkstyle:JavadocVariable")
     private int height;
+
+
+    @SuppressWarnings("checkstyle:JavadocVariable")
+    private Rectangle safeArea;
 
 
     /**
@@ -26,10 +30,14 @@ public class CanvasContext {
      * @param width The pixel-width of the represented Canvas.
      * @param height The pixel-height of the represented Canvas.
      */
+    @SuppressWarnings("checkstyle:HiddenField")
     public CanvasContext(final int width, final int height) {
         this.width = width;
         this.height = height;
+
+        this.safeArea = new Rectangle(0, 0, width, height);
     }
+
 
     /**
      * @return The pixel-width of the Canvas represented by this Context-object.
@@ -38,10 +46,36 @@ public class CanvasContext {
         return width;
     }
 
+
     /**
      * @return The pixel-height of the Canvas represented by this Context-object.
      */
     public int getHeight() {
         return height;
     }
+
+
+    /**
+     * Provides information about the area that can be used for underlying Renderers for drawing.
+     *
+     * @return the Rectangle considered the safeArea for underlying renderers.
+     */
+    public Rectangle getSafeArea() {
+        return safeArea;
+    };
+
+
+    /**
+     * Updates this safeArea with the location and dimension found in the specified Rectangle.
+     *
+     * @param rect the specified Rectangle.
+     *
+     * @return this CanvasContext.
+     */
+    public CanvasContext updateSafeArea(final Rectangle rect) {
+        safeArea.updateFrom(rect);
+        return this;
+    }
+
+
 }
