5593dadab867b1490ae24e6051a4d123db6d2821
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-20 18:07:09 +0200

    chore: add package-info for gfx-package

---
+++ src/main/java/libjam/gfx/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Graphics package. Contains rendering and utility functions for rendering purposes.
+ *
+ * @since 1.0
+ * @author Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
+ */
+
+package libjam.gfx;
