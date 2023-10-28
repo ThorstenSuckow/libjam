b541e2b1ea25d9a6e0c73cf8500451f97afd03fc
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-28 23:24:56 +0200

    feat: add default implementation for GfxParent

---
+++ src/main/java/dev/libjam/gfx/drawable/DefaultGfxParent.java
@@ -0,0 +1,127 @@
+package dev.libjam.gfx.drawable;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class DefaultGfxParent extends GfxParentBase {
+
+    @SuppressWarnings("checkstyle:JavadocVariable")
+    private static final Color BG_COLOR = Color.BLACK;
+
+    @SuppressWarnings("checkstyle:JavadocVariable")
+    private static final Color BORDER_COLOR = Color.CYAN;
+
+    @SuppressWarnings("checkstyle:JavadocVariable")
+    private Color bgColor;
+
+    @SuppressWarnings("checkstyle:JavadocVariable")
+    private Color borderColor;
+
+
+    /**
+     * Creates a new GfxDrawable with its height, width, x- and y-coordinate set to 0.
+     */
+    public DefaultGfxParent() {
+        this(0, 0,0,0);
+    }
+
+
+    /**
+     * Creates a new GfxDrawable with its width and height set to the specified values,
+     * the x- and y-coordinate set to 0.
+     *
+     * @param width the specified width
+     * @param height the specified height
+     */
+    public DefaultGfxParent(final double width, final double height) {
+        this(0,0, width, height);
+    }
+
+
+    /**
+     * Creates a new GfxDrawable with its width, height, x- and y-coordinate
+     * set to the specified values.
+     *
+     * @param width the specified width
+     * @param height the specified height
+     * @param x the specified x-coordinate
+     * @param y the specified y-coordinate
+     */
+    public DefaultGfxParent(final double x, final double y, final double width, final double height) {
+        this(x, y, width, height, null);
+    }
+
+    /**
+     * Creates a new GfxDrawable with its width, height, x- and y-coordinate
+     * set to the specified values.
+     *
+     * @param width the specified width
+     * @param height the specified height
+     * @param x the specified x-coordinate
+     * @param y the specified y-coordinate
+     */
+    @SuppressWarnings("checkstyle:FinalParameters")
+    public DefaultGfxParent(final double x, final double y, final double width, final double height, Color borderColor) {
+        super(x, y, width, height);
+        this.borderColor = borderColor;
+    }
+
+    public GfxNode setBgColor (final Color bgColor) {
+        this.bgColor = bgColor;
+        return this;
+    }
+
+    public GfxNode setBorderColor (final Color borderColor) {
+        this.borderColor = borderColor;
+        return this;
+    }
+
+
+    @Override
+    protected GfxNode drawNode(final GraphicsContext g) {
+
+        if (bgColor != null) {
+            g.setFill(bgColor);
+            g.fillOval(
+                xProperty().get(),
+                yProperty().get(),
+                widthProperty().get(),
+                heightProperty().get()
+            );
+        }
+
+        if (borderColor != null) {
+            g.setStroke(borderColor);
+            g.strokeOval(
+                xProperty().get(),
+                yProperty().get(),
+                widthProperty().get(),
+                heightProperty().get()
+            );
+        }
+
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return "GfxParent["
+            + "x:" + x
+            + "; y:" + y
+            + "; width:" + width
+            + "; height: " + height
+            + "]";
+    }
+
+    @Override
+    public GfxParent add(final GfxNode node) throws IllegalArgumentException {
+        if (node.getParent() != null) {
+            throw new IllegalArgumentException();
+        }
+
+        node.setParent(this);
+        children.add(node);
+        return this;
+    }
+}
---
+++ src/test/java/dev/libjam/gfx/drawable/DefaultGfxParentTest.java
@@ -0,0 +1,146 @@
+package dev.libjam.gfx.drawable;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class DefaultGfxParentTest {
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    @Test
+    @DisplayName("DefaultGfxParent")
+    void testDefaultGfxParent() {
+
+        final int width = 800;
+        final int height = 600;
+        final int x = 1;
+        final int y = 2;
+
+        DefaultGfxParent p = new DefaultGfxParent();
+
+        Assertions.assertInstanceOf(GfxParentBase.class, p);
+
+        Assertions.assertEquals(0,  p.getWidth());
+        Assertions.assertEquals(0,  p.getHeight());
+        Assertions.assertEquals(0,  p.getX());
+        Assertions.assertEquals(0,  p.getY());
+
+        // setter
+        Assertions.assertSame(p, p.setWidth(width).setHeight(height).setX(x).setY(y));
+        Assertions.assertEquals(width,  p.getWidth());
+        Assertions.assertEquals(height,  p.getHeight());
+        Assertions.assertEquals(x,  p.getX());
+        Assertions.assertEquals(y,  p.getY());
+
+
+        p = new DefaultGfxParent(width, height);
+        Assertions.assertEquals(width,  p.getWidth());
+        Assertions.assertEquals(height,  p.getHeight());
+        Assertions.assertEquals(0,  p.getX());
+        Assertions.assertEquals(0,  p.getY());
+
+        p = new DefaultGfxParent(x, y, width, height);
+        Assertions.assertEquals(width,  p.getWidth());
+        Assertions.assertEquals(height,  p.getHeight());
+        Assertions.assertEquals(x,  p.getX());
+        Assertions.assertEquals(y,  p.getY());
+
+        Assertions.assertEquals(p.widthProperty().get(), p.getWidth());
+        final double newWidth = 47;
+        p.widthProperty().set(47);
+        Assertions.assertEquals(newWidth, p.getWidth());
+    }
+
+
+    @Test
+    @DisplayName("visible = false -> draw()")
+    void testDrawVisibleFalse() {
+
+        DefaultGfxParent pSpy = spy(new DefaultGfxParent());
+
+        // not visible
+        Assertions.assertFalse(pSpy.isVisible());
+        Assertions.assertFalse(pSpy.visibleProperty().get());
+
+
+        // make sure colors are set
+        pSpy.setBgColor(Color.GREEN);
+        pSpy.setBorderColor(Color.BLUE);
+
+
+        // mock Graphics
+        GraphicsContext g = mock(GraphicsContext.class);
+
+        // draw - visible false
+        when(pSpy.drawNode(g)).thenThrow(new AssertionError());
+        when(pSpy.drawChildren(g)).thenThrow(new AssertionError());
+
+        pSpy.draw(g);
+
+        // draw - visible true
+        reset(pSpy);
+        pSpy.setVisible(true);
+        AtomicBoolean drawn = new AtomicBoolean(false);
+        when(pSpy.drawNode(g)).thenAnswer((Answer) invocation -> {drawn.set(true); return null;});
+        pSpy.draw(g);
+        Assertions.assertTrue(drawn.get());
+
+    }
+
+    @Test
+    @DisplayName("add")
+    void testAdd() {
+
+        DefaultGfxParent h1 = new DefaultGfxParent();
+        DefaultGfxParent d = new DefaultGfxParent();
+
+        DefaultGfxNode n = new DefaultGfxNode();
+
+        h1.add(n);
+
+        Assertions.assertSame(h1, n.getParent());
+
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> d.add(n));
+
+    }
+
+    @Test
+    @DisplayName("children")
+    void testChildren() {
+
+        DefaultGfxParent d = new DefaultGfxParent();
+        DefaultGfxParent d1 = new DefaultGfxParent();
+
+        DefaultGfxNode d1_1 = new DefaultGfxNode();
+        DefaultGfxNode d1_2 = new DefaultGfxNode();
+
+        d.add(d1);
+
+        List<GfxNode> d1Nodes_1 = d1.getChildren();
+
+        d1.add(d1_1);
+        d1.add(d1_2);
+
+        // make sure children returned is copy
+        List<GfxNode> d1Nodes_2 = d1.getChildren();
+        Assertions.assertNotSame(d1Nodes_1, d1Nodes_2);
+
+
+        Assertions.assertEquals(2, d1.getChildren().size());
+        Assertions.assertTrue(d1.getChildren().contains(d1_1));
+        Assertions.assertTrue(d1.getChildren().contains(d1_2));
+
+    }
+
+}
