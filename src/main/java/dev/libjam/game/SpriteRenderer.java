51fc9452aeb9984356c31e81e68fd4be6d9ef711
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-11-15 23:55:42 +0100

    chore(docs): add doc blocks

--- src/main/java/dev/libjam/game/SpriteRenderer.java
+++ src/main/java/dev/libjam/game/SpriteRenderer.java
@@ -2,8 +2,19 @@ package dev.libjam.game;
 
 import javafx.scene.image.WritableImage;
 
+
+/**
+ * Functional interface for producing images for Sprites.
+ */
 public interface SpriteRenderer {
 
+    /**
+     * Returns a visual representation of the specified Sprite as an image.
+     *
+     * @param sprite the specified Sprite for which an image gets produced.
+     *
+     * @return the produced WritableImage representing the specified Sprite.
+     */
     WritableImage render(Sprite sprite);
 
 }
