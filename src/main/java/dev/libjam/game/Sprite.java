c4657044577bcb0f9701b3e776e0dbacd47732be
ThorstenSuckow <thorsten@suckow-homberg.de>
2023-11-15 09:41:02 +0100

    refactor: update Sprite and add test

--- src/main/java/dev/libjam/game/Sprite.java
+++ src/main/java/dev/libjam/game/Sprite.java
@@ -1,7 +1,5 @@
 package dev.libjam.game;
 
-import dev.libjam.game.event.ObjectLifecycleEvent;
-import dev.libjam.game.event.ObjectLifecycleListener;
 import dev.libjam.gfx.drawable.DefaultGfxNode;
 import dev.libjam.gfx.drawable.GfxNode;
 import dev.libjam.physx.Object2D;
@@ -9,39 +7,100 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.WritableImage;
 
 import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
 
+
+/**
+ * A Sprite is a visual representation of an Object2D.
+ * The initial LifecycleState of a Sprite is PREPARING.
+ */
+@SuppressWarnings("checkstyle:LineLength")
 public class Sprite extends DefaultGfxNode {
 
-    private Object2D obj;
+    @SuppressWarnings("checkstyle:JavadocVariable")
+    private Object2D object2D;
 
+    @SuppressWarnings("checkstyle:JavadocVariable")
     private SpriteRenderer renderer;
 
+    @SuppressWarnings("checkstyle:JavadocVariable")
     private WritableImage img;
 
-
+    @SuppressWarnings("checkstyle:JavadocVariable")
     private LifecycleState state;
 
-    public Sprite(Object2D obj) {
+    private List<PropertyChangeListener> propertyChangeListener = new ArrayList<>();
 
+    /**
+     * Creates a new Sprite representing the specified Object2D.
+     *
+     * @param object2D the specified Object2D this Sprite should represent.
+     */
+    @SuppressWarnings("checkstyle:HiddenField")
+    public Sprite(final Object2D object2D) {
+        this(object2D, null);
     }
 
-    public Sprite(Object2D obj, SpriteRenderer renderer) {
 
-        this.obj = obj;
-        this.setX(obj.getX());
-        this.setWidth(obj.getWidth()).setHeight(obj.getHeight());
-        obj.addPropertyChangeListener(this::object2DPropertyChange);
+    /**
+     * Creates a new Sprite representing the specified Object2D using
+     * the specified renderer
+     *
+     * @param object2D The specified Object2D
+     * @param renderer The specified renderer.
+     */
+    @SuppressWarnings("checkstyle:HiddenField")
+    public Sprite(
+            final Object2D object2D,
+            final SpriteRenderer renderer
+    ) {
+        this.object2D = object2D;
         this.renderer = renderer;
-    }
-
-    public void prepareImg() {
-        if (this.renderer != null) {
-            // img = this.renderer.render(this);
-        }
+        state = LifecycleState.PREPARING;
     }
 
 
+    /**
+     * Returns the Object2D this Sprite represents.
+     *
+     * @return the Object2D this Sprite represents.
+     */
+    public Object2D getObject2D() {
+        return object2D;
+    }
 
+
+    /**
+     * Returns the SpriteRenderer for this Sprite.
+     *
+     * @return the SpriteRender used for this Sprite. null, if none was registered
+     */
+    public SpriteRenderer getRenderer() {
+        return renderer;
+    }
+
+
+    /**
+     * Returns the max age of this Sprite.
+     *
+     * @return -1 if there is no max-age, otherwise the max-age in
+     * nanoseconds.
+     */
+    public long getMaxAge() {
+        return -1;
+    }
+
+
+    /**
+     * Requests an image from this Sprite's renderer for drawing
+     * using the specified GraphicsContext.
+     *
+     * @param g The GraphicsContext that should be used for rendering.
+     *
+     * @return this Sprite as a GfxNode
+     */
     @Override
     protected GfxNode drawNode(final GraphicsContext g) {
 
@@ -53,65 +112,95 @@ public class Sprite extends DefaultGfxNode {
             g.drawImage(img, getX(), getY());
         }
 
-
         return this;
     }
 
 
-    public void object2DPropertyChange(PropertyChangeEvent evt) {
-        if (evt.getPropertyName() == "y") {
-            double y = (Double)evt.getNewValue();
-            setY(y);
-        }
-
-        if (evt.getPropertyName() == "x") {
-            double x = (Double)evt.getNewValue();
-            setX(x);
-        }
-    }
-
-
-
-
-    public long getMaxAge() {
-        return -1;
-    }
-
-    public long getAge(long time) {
-        return obj.getAge(time);
-    }
-
-    public long getAge() {
-        return obj.getAge();
-    }
-
-    public boolean shouldExpire(long time) {
-        if (getMaxAge() == -1) {
-            return false;
-        }
-        return getAge(time) > getMaxAge();
-    }
-
-    public void expire () {
-        this.state = LifecycleState.EXPIRED;
-    }
-
-    public void spawn() {
-        this.state = LifecycleState.SPAWNED;
-    }
-
+    /**
+     * Returns the current LifecycleState of this Sprite.
+     *
+     * @return the LifecycleState of this Sprite.
+     */
     public LifecycleState getState() {
         return state;
     }
 
-   /* public void addObjectLifecycleListener(ObjectLifecycleListener listener) {
-        objectLifecycleListeners.add(listener);
+
+
+    /**
+     * Sets the state of this Sprite to the specified LifecycleState.
+     * Fires a PropertyChangeEvent if a new valid state is detected.
+     * A state may be reset from EXPIRING to LIVING.
+     *
+     * @param state The specified LifecycleState.
+     *
+     * @throws IllegalArgumentException if the ordinal of the new LifecycleState
+     * is less than the previous one and the old state is not EXPIRING and new state
+     * is not LIVING.
+     */
+    @SuppressWarnings("checkstyle:HiddenField")
+    public void setState(final LifecycleState state) throws IllegalArgumentException {
+
+        if (state.ordinal() < this.state.ordinal() && state != LifecycleState.LIVING
+        && this.state != LifecycleState.EXPIRING) {
+            throw new IllegalArgumentException("state \"" + state + "\" not allwed");
+        }
+
+        if (state.ordinal() == this.state.ordinal()) {
+            return;
+        }
+
+        this.state = state;
+
+        firePropertyChange("state", this.state, state);
     }
 
-    protected void fireObjectLifecycleEvent(Object2D source, LifecycleState state) {
-        for (ObjectLifecycleListener lst: objectLifecycleListeners) {
-            lst.objectLifecycleChange(new ObjectLifecycleEvent(source, state));
+
+    /**
+     * Fires a PropertyChangeEvent based on the specified values.
+     *
+     *
+     * @param propertyName the name of the property that changed.
+     * @param oldValue The previous value of the property.
+     * @param newValue The new value of the property.
+     */
+    private void firePropertyChange(
+        final String propertyName,
+        final Object oldValue,
+        final Object newValue
+    ) {
+        PropertyChangeEvent evt = new PropertyChangeEvent(
+                this, propertyName, oldValue, newValue
+        );
+
+        for (int i = 0; i < propertyChangeListener.size(); i++) {
+            propertyChangeListener.get(i).propertyChange(evt);
         }
-    }*/
+    }
+
+
+    /**
+     * Adds the specified PropertyChangeListener to the list of this Sprite's listeners.
+     *
+     * @param listener The specified PropertyChangeListener to add.
+     */
+    public void addPropertyChangeListener(final PropertyChangeListener listener) {
+
+        if (!propertyChangeListener.contains(listener)) {
+            propertyChangeListener.add(listener);
+        }
+    }
+
+
+    /**
+     * Removes the specified PropertyChangeListener from this Sprite's
+     * list of PropertyChangeListeners.
+     *
+     * @param listener The specified PropertyChangeListener to remove.
+     */
+    public void removePropertyChangeListener(final PropertyChangeListener listener) {
+        propertyChangeListener.remove(listener);
+    }
+
 
 }
